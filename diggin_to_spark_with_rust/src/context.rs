b0a0d13964cc147c3be8d21149fd7f13099708b9
secureWKkim <1rladnjsrud1@gmail.com>
2023-08-23 16:48:13 +0900

    rust essentials md files

--- diggin_to_spark_with_rust/src/context.rs
+++ diggin_to_spark_with_rust/src/context.rs
@@ -1,7 +1,5 @@
 use std::fs::File;
 
-//TODO: String 스택/힙 할당 & 5.1 맨 밑 구조체의 소유권 내용 다루기.
-
 pub struct SparkContext {
     pub master: String,
     pub py_files: Vec<File>
--- diggin_to_spark_with_rust/src/main.rs
+++ diggin_to_spark_with_rust/src/main.rs
@@ -3,8 +3,8 @@ mod context;
 use context::{SparkContext};
 
 fn main() {
-    let sc = SparkContext {
-        master: String::from("localhost:8080"),
+    let sc = SparkContext { //mut 아니다.
+        master: String::from("localhost:8080"),//TODO: String 스택/힙 할당 & 5.1 맨 밑 구조체의 소유권 내용 다루기.
         py_files: Vec::new(),
     };
     println!("Hello, world!");
